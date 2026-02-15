7ec3afc453c330204d99fe2d2ded72140d50b351
CL3372 <chocolatesdomjose@gmail.com>
2026-02-15 18:00:23 +0000

    added HTML java and added typewriter plugging in

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  let recipeElement = document.querySelector("#recipe");
+
+  new Typewriter("#recipe", {
+    strings: "Dark Chocolate",
+    autoStart: true,
+    delay: 1.5,
+    cursor: "",
+  });
+
+  alert("generating recipe");
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
